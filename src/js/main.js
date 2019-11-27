69f6e441e01aecb27ed6cf502cd1339e28382391
JordyForNow <s1114360@student.windesheim.nl>
2019-11-27 13:32:09 +0100

    Worked on code

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,25 @@
 var code;
 
 var workspace = Blockly.inject('blocklyDiv',
-    {toolbox: document.getElementById('toolbox')});
+    {toolbox: document.getElementById('toolbox'),
+    grid:
+         {spacing: 30,
+          length: 2,
+          colour: '#ccc',
+          snap: false},
+    zoom:
+         {controls: true,
+          wheel: true,
+          startScale: 1.0,
+          maxScale: 3,
+          minScale: 0.3,
+          scaleSpeed: 1.2},
+    trashcan: true});
 
-function myUpdateFunction(event) {
-    let a = Blockly.JavaScript.workspaceToCode(workspace);
-    if (!(a == null || a === "")) {
-        document.getElementById('output').value = a;
-    }
-}
-workspace.addChangeListener(myUpdateFunction);
+// function myUpdateFunction(event) {
+//     let a = Blockly.JavaScript.workspaceToCode(workspace);
+//     if (!(a == null || a === "")) {
+//         document.getElementById('output').value = a;
+//     }
+// }
+// workspace.addChangeListener(myUpdateFunction);
